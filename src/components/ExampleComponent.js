28fe9c3a35c9d61c936d8949ecdff834c11feb81
Barto <juanmartin.cerruti@gmail.com>
2021-10-14 10:29:01 -0300

    first commit exercises

---
+++ src/components/ExampleComponent.js
@@ -0,0 +1,11 @@
+/* eslint-disable */
+import React from 'react';
+
+export const ExampleComponent = (props) => {
+
+    return (
+        <div className="ExampleComponent">
+            Hello
+        </div>
+    )
+}
